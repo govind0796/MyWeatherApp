b53be36c1e2abe2fc6d5e3143654bd9dcc3caa83
govind0796 <48519548+govind0796@users.noreply.github.com>
2023-07-03 23:00:13 +0530

    Update Program.cs

--- MyWeatherApp/Program.cs
+++ MyWeatherApp/Program.cs
@@ -11,49 +11,74 @@ namespace MyWeatherApp
         
         public static async Task Main()
         {
-            //NOTE : Change connection string as per your server
             const string connectionString = @"Data Source=GOVIND-OJHA\SQLEXPRESS;Initial Catalog=MyWeatherAppDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            
+
             await using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
 
-            Console.Write("Enter city name: ");
-
-            var cityName = Console.ReadLine();
-
-            if (!string.IsNullOrEmpty(cityName))
+            while (true)
             {
-                cityName = cityName.ToLower();
-
-                const string query = "SELECT * FROM tbl_worldCitiesCoordinates WHERE cityName = @cityName;";
-                await using var command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@cityName", cityName);
-
-                await using var reader = await command.ExecuteReaderAsync();
-
-                var dataTable = new DataTable();
-                dataTable.Load(reader);
-
-                if (dataTable.Rows.Count > 0)
+                Console.Write("Enter city name (Enter 0 to exit): ");
+                var cityName = Console.ReadLine();
+                if (cityName == "0")
                 {
-                    var latitude = (double)dataTable.Rows[0]["Latitude"];
-                    var longitude = (double)dataTable.Rows[0]["Longitude"];
+                    break;
+                }
 
-                    var weatherService = new WeatherServiceApi();
-                    var weatherData = await weatherService.GetWeatherData(latitude, longitude);
+                if (!string.IsNullOrEmpty(cityName) && !string.IsNullOrWhiteSpace(cityName))
+                {
+                    cityName = cityName.Trim();
+                    var hasNumber = ContainsNumber(cityName);
+                    if (hasNumber)
+                    {
+                        Console.Write("Numbers not allowed");
+                    }
+                    else
+                    {
+                        cityName = cityName.ToLower();
 
-                    var weatherDataJson = JsonConvert.DeserializeObject<WeatherData>(weatherData);
+                        const string query = "SELECT * FROM tbl_worldCitiesCoordinates WHERE cityName = @cityName;";
+                        await using var command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@cityName", cityName);
 
+                        await using var reader = await command.ExecuteReaderAsync();
 
-                    Console.WriteLine($"Current weather report of {cityName.ToUpper()}");
-                    Console.WriteLine($"Temperature = {weatherDataJson.current_weather.temperature} degree Celsius");
-                    Console.WriteLine($"Wind Speed = {weatherDataJson.current_weather.windspeed} km/hr");
+                        var dataTable = new DataTable();
+                        dataTable.Load(reader);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            var latitude = (double)dataTable.Rows[0]["Latitude"];
+                            var longitude = (double)dataTable.Rows[0]["Longitude"];
+
+                            var weatherService = new WeatherServiceApi();
+                            var weatherData = await weatherService.GetWeatherData(latitude, longitude);
+
+                            var weatherDataJson = JsonConvert.DeserializeObject<WeatherData>(weatherData);
+
+                            // Print the data fetched from the API here
+                            Console.WriteLine($"Current weather report of {cityName.ToUpper()}");
+                            Console.WriteLine($"Temperature = {weatherDataJson.CurrentWeather.Temperature} degree Celsius");
+                            Console.WriteLine($"Wind Speed = {weatherDataJson.CurrentWeather.WindSpeed} km/hr");
+                        }
+                        else
+                        {
+                            Console.WriteLine("City not found.");
+                        }
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("City not found.");
+                    Console.WriteLine("No data entered. Please try again.");
                 }
+
+                Console.WriteLine();
             }
+        }
+
+        public static bool ContainsNumber(string input)
+        {
+            return input.Any(char.IsDigit);
         }
     }
 }

5a8a1098b86085ef04844c3c20ad62eb13a9e891
govind0796 <48519548+govind0796@users.noreply.github.com>
2023-07-03 23:12:52 +0530

    Update Program.cs

--- MyWeatherApp/Program.cs
+++ MyWeatherApp/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Data;
+using System.Linq;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-using Newtonsoft.Json;
 
 namespace MyWeatherApp
 {
